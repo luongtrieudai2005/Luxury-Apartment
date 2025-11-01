bb8c93b4e54e660b9573180b4b0ab4dc957c5fa9
luongtrieudai2005 <luongtrieudai32@gmail.com>
2025-11-01 11:11:42 +0700

    Upload first Project PTTKYC

---
+++ config.py
@@ -0,0 +1,6 @@
+﻿import os
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "secret-key")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///rent_management.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
